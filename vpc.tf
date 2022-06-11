491136a628874da469a2679ab3c6fc0a33e78bd6
George Black <contact@george.black>
2022-06-10 21:58:58 -0500

    Add subnets

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,46 @@ resource "aws_vpc" "oceanblue" {
     "Name" = "oceanblue-vpc"
   }
 }
+
+resource "aws_subnet" "oceanblue_subnet_1a" {
+  vpc_id                          = aws_vpc.oceanblue.id
+  cidr_block                      = "10.0.1.0/24"
+  availability_zone               = "us-east-1a"
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    Name = "oceanblue-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "oceanblue_subnet_1a" {
+  vpc_id            = aws_vpc.oceanblue.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "oceanblue-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "oceanblue_subnet_1b" {
+  vpc_id            = aws_vpc.oceanblue.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "oceanblue-subnet-1b"
+  }
+}
+
+resource "aws_subnet" "oceanblue_subnet_1c" {
+  vpc_id            = aws_vpc.oceanblue.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "oceanblue-subnet-1c"
+  }
+}
+
+

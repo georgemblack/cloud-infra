ce66046133c6dec5477e406ce42929dd656ee5c1
George Black <contact@george.black>
2022-06-10 21:25:53 -0500

    Add VPC

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "oceanblue" {
+  cidr_block                       = "10.0.0.0/16"
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = true
+
+  tags = {
+    "Name" = "oceanblue-vpc"
+  }
+}

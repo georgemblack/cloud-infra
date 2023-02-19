0d440e62bc2f2297c7fef82790ceecf0413382fa
George Black <contact@george.black>
2023-02-19 14:52:59 -0600

    Updates to web project

--- projects/web/README.md
+++
@@ -1,8 +0,0 @@
-# Web Infra
-
-```
-terraform workspace select staging
-terraform apply -var-file=vars/staging.tfvars
-terraform workspace select production
-terraform apply -var-file=vars/production.tfvars
-```
--- projects/web/main.tf
+++ projects/web/main.tf
@@ -5,11 +5,11 @@ terraform {
   }
 }
 
-resource "google_storage_bucket" "assets" {
-  name                        = var.bucket_name
+resource "google_storage_bucket" "assets_staging" {
+  name                        = "web-staging.george.black"
   location                    = "US"
   force_destroy               = true
-  project                     = var.project
+  project                     = "oceanblue-web"
   uniform_bucket_level_access = true
 
   versioning {
@@ -17,28 +17,59 @@ resource "google_storage_bucket" "assets" {
   }
 }
 
+resource "google_storage_bucket" "assets" {
+  name                        = "web.george.black"
+  location                    = "US"
+  force_destroy               = true
+  project                     = "oceanblue-web"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+# Service account for Transmit app on the Mac
 resource "google_service_account" "transmit" {
   account_id   = "transmit"
   display_name = "Transmit"
   description  = "Used by Transmit for Mac to sync assets to Google Cloud Storage"
-  project      = var.project
+  project      = "oceanblue-web"
 }
 
+# Enable Transmit to Google Cloud Storage
 resource "google_project_iam_member" "transmit" {
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.transmit.email}"
-  project = var.project
+  project = "oceanblue-web"
 }
 
+# Service account for GitHub Codespaces
 resource "google_service_account" "codespaces" {
   account_id   = "github-codespaces"
   display_name = "GitHub Codespaces"
   description  = "Used by GitHub Codespaces for development"
-  project      = var.project
+  project      = "oceanblue-web"
 }
 
+# Enable Codespaces general project access
 resource "google_project_iam_member" "codespaces" {
   role    = "roles/editor"
   member  = "serviceAccount:${google_service_account.codespaces.email}"
-  project = var.project
+  project = "oceanblue-web"
+}
+
+# Service account for Web Builder service
+resource "google_service_account" "web_builder" {
+  account_id   = "web-builder"
+  display_name = "Builder"
+  description  = "Used by Web Builder Cloud Run service"
+  project      = "oceanblue-web"
+}
+
+# Enable Web Builder to access secrets
+resource "google_project_iam_member" "web_builder" {
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.web_builder.email}"
+  project = "oceanblue-web"
 }
